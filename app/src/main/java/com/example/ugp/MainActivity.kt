fefbd8ae5f4f6de228e5cf19105f48203abbaa5e
mehtameet2802 <mehtameetoff2802@gmail.com>
2021-10-24 18:48:29 +0530

    Meet commit 1

--- app/src/main/java/com/example/ugp/MainActivity.kt
+++ app/src/main/java/com/example/ugp/MainActivity.kt
@@ -1,11 +1,144 @@
 package com.example.ugp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat
+import com.bumptech.glide.Glide
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    // variables for side navigation
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var drawer: DrawerLayout
+    private lateinit var side_nav: NavigationView
+    lateinit var toolbar: Toolbar
+
+
+    //variables for signout
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    val mAuth = Firebase.auth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //variables for assigning image,name and emailid
+        var image = findViewById<ImageView>(R.id.nav_image)
+        var name = findViewById<TextView>(R.id.nav_name)
+        var email = findViewById<TextView>(R.id.nav_email)
+
+
+        // assigning values to information variables
+        email.text = mAuth.currentUser!!.email
+        name.text = mAuth.currentUser!!.displayName
+        if (mAuth.currentUser!!.photoUrl != null) {
+            val url = mAuth.currentUser!!.photoUrl
+            Glide.with(this)
+                .load(url)
+                .into(image)
+        }
+
+        //assigning variables of side nav
+        side_nav = findViewById(R.id.side_nav)
+        drawer = findViewById(R.id.drawer_layout)
+        toolbar = findViewById(R.id.app_bar)
+
+
+        //setting action bar for side navigation
+        setSupportActionBar(toolbar)
+        toggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        //toggle.isDrawerIndicatorEnabled = true
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+
+        //setting onClick for side nav options
+
+        side_nav.setNavigationItemSelectedListener {
+            drawer.closeDrawer(GravityCompat.START)
+            when (it.itemId) {
+                R.id.home -> {
+                    // this will take to main activity
+                    val i = Intent(this, MainActivity::class.java)
+                    startActivity(i)
+                    finish()
+                }
+                R.id.profile -> {
+                    // this will take to profile activity
+                    val i = Intent(this, ProfileActivity::class.java)
+                    startActivity(i)
+                    finish()
+                }
+                R.id.logout -> {
+                    // This will show a dialog box foe logging out
+                    val builder = this.let { it1 -> AlertDialog.Builder(it1) }
+                    builder.setTitle("Exit/Logout")
+                    builder.setMessage("Do you really want to exit \n You will be logged out")
+                    builder.setPositiveButton("Yes") { dialog, which ->
+                        signOut()
+                        val intent = Intent(
+                            this,
+                            LoginActivity::class.java
+                        )   //Please add the login activity name
+                        startActivity(intent)
+                        this.finish()
+                    }
+                    builder.setNegativeButton("No") { dialog, which ->
+                        Toast.makeText(this, "Thank you for staying", Toast.LENGTH_SHORT).show()
+
+                    }
+                    builder.setNeutralButton("Cancel") { dialog, which ->
+                        Toast.makeText(this, "Thank you for staying", Toast.LENGTH_SHORT).show()
+                    }
+
+                    val dialog: AlertDialog = builder.create()
+                    dialog.show()
+
+                }
+            }
+            true
+        }
+
     }
+
+    // for opening side_nav
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    // function for signing out
+    private fun signOut() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        Toast.makeText(this, "You have been successfully signed out", Toast.LENGTH_SHORT).show()
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        mGoogleSignInClient.signOut()
+
+        Firebase.auth.signOut()
+    }
+
 }
