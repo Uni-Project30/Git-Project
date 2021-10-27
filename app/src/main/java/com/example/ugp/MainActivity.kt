3f0e0345d37ca394b9eb1aae3ec42960b46879bf
Abhijit Saha <asoksha715@gmail.com>
2021-10-27 21:48:24 +0530

    Initial Right Nav

--- app/src/main/java/com/example/ugp/MainActivity.kt
+++ app/src/main/java/com/example/ugp/MainActivity.kt
@@ -9,7 +9,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import com.bumptech.glide.Glide
 import androidx.drawerlayout.widget.DrawerLayout
